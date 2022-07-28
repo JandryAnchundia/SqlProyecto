ef1e4069cca205a56861694f34bc89ca04d1b25a
Jandry_Anchundia <jandryorlando311098@gmail.com>
2022-07-27 21:31:58 -0500

    inner join agg

--- alumnosScriptVistas.sql
+++ alumnosScriptVistas.sql
@@ -4,29 +4,34 @@ use alumnos;
 -- ---------------------------------------
 -- Creacion de vitas
 -- ---------------------------------------
--- Vista sobre todas las materias que cursa un alumno
-create view alumnoMaterias as 
-select nombreMateria from materia;
-select * from alumnoMaterias ;
+-- Vista sobre la carrera que sigue un alumno
+create or replace view alumnoCarreras as 
+select alumno.nombre, carrera.duracionCarrera, carrera.nombreCarrera
+from alumno
+inner join carrera on alumno.idCarrera = carrera.id;
+select * from alumnoCarreras ;
 
--- Vista sobre el nombre y la carrera que cursa el alumno 
-create view carreraAlumnos as 
-select nombre, semestre, nombreCarrera from alumno, carrera;
-select * from carreraAlumnos;
 
 -- Vista sobre informacion del alumno
-create view informacionAlumnos as 
+create or replace view informacionAlumnos as 
 select id, nombre, genero, edad from alumno;
 select * from informacionAlumnos;
 
+
 -- Vista sobre los alumnos que tienen becas
-create view informacionAlumnosBecas as 
-select nombre, tipoBeca, nombreCarrera from alumno, becas, carrera;
-select * from informacionAlumnosBecas;
+create or replace view informacionAlumnosBeca as 
+select alumno.nombre, becas.tipoBeca 
+from alumno, becas
+inner join alumnobecas on alumnobecas.idBecas = alumnoBecas.idAlumno;
+select * from informacionAlumnosBeca;
+
+
+
 
 -- Vista sobre los profesores y los alumnos
-create view alumnoProfesor as
-select nombre as nombreAlumno, nombreProfesor  from alumno, profesor;
+create or replace view alumnoProfesor as
+select nombre as nombreAlumno, nombreProfesor  
+from alumno, profesor;
 select * from alumnoProfesor;
 
 
