f2a289593f7f61da70ef9157a5a9a299cfc7791d
Jandry_Anchundia <jandryorlando311098@gmail.com>
2022-08-15 21:50:53 -0500

    Realizado

---
+++ alumnosScriptControlTransacciones.sql
@@ -0,0 +1,93 @@
+#Eliminar registros y recuperarlos
+
+START TRANSACTION;
+	DELETE FROM alumnos.alumnomateria WHERE idMateria = 1;
+    DELETE FROM alumnos.alumnomateria WHERE idMateria  = 2;
+	DELETE FROM alumnos.alumnomateria WHERE idMateria  = 3;
+	DELETE FROM alumnos.alumnomateria WHERE idMateria  = 4;
+	DELETE FROM alumnos.alumnomateria WHERE idMateria  = 5;
+
+
+#Para deshacer las eliminaciones ejecuto:
+ROLLBACK;
+
+#Eliminar registros definitivamente
+
+START TRANSACTION;
+	DELETE FROM alumnos.alumnobecas WHERE idAlumno = 1;
+    DELETE FROM alumnos.alumnobecas WHERE idAlumno = 2;
+	DELETE FROM alumnos.alumnobecas WHERE idAlumno = 3;
+	DELETE FROM alumnos.alumnobecas WHERE idAlumno = 4;
+	DELETE FROM alumnos.alumnobecas WHERE idAlumno = 5;
+#Para confirmar definitivamente las eliminaciones ejecuto:
+COMMIT
+
+
+
+
+
+#Eliminar registros y recuperarlos
+
+START TRANSACTION;
+	DELETE FROM alumnos.alumnomateria WHERE idMateria = 1;
+    DELETE FROM alumnos.alumnomateria WHERE idMateria  = 2;
+	DELETE FROM alumnos.alumnomateria WHERE idMateria  = 3;
+	DELETE FROM alumnos.alumnomateria WHERE idMateria  = 4;
+	DELETE FROM alumnos.alumnomateria WHERE idMateria  = 5;
+
+
+#Para deshacer las eliminaciones ejecuto:
+ROLLBACK;
+
+#Eliminar registros definitivamente
+
+START TRANSACTION;
+	DELETE FROM alumnos.alumnobecas WHERE idAlumno = 1;
+    DELETE FROM alumnos.alumnobecas WHERE idAlumno = 2;
+	DELETE FROM alumnos.alumnobecas WHERE idAlumno = 3;
+	DELETE FROM alumnos.alumnobecas WHERE idAlumno = 4;
+	DELETE FROM alumnos.alumnobecas WHERE idAlumno = 5;
+#Para confirmar definitivamente las eliminaciones ejecuto:
+COMMIT;
+
+
+
+#Inserción de 8 nuevos registros con savepoint
+START TRANSACTION;
+	INSERT INTO alumnos.alumnomateria (`id`,`idAlumno`,`idMateria`,`promedioParcial_1`,`promedioParcial_2`) 
+    VALUES ('0','1','2',10,10);
+    INSERT INTO alumnos.alumnomateria (`id`,`idAlumno`,`idMateria`,`promedioParcial_1`,`promedioParcial_2`) 
+    VALUES ('0','2','2',10,10);
+    INSERT INTO alumnos.alumnomateria (`id`,`idAlumno`,`idMateria`,`promedioParcial_1`,`promedioParcial_2`) 
+    VALUES ('0','3','2',10,10);
+    INSERT INTO alumnos.alumnomateria (`id`,`idAlumno`,`idMateria`,`promedioParcial_1`,`promedioParcial_2`) 
+    VALUES ('0','4','2',10,10);
+	
+SAVEPOINT CuatroRegistros;
+	INSERT INTO alumnos.alumnobecas (`id`,`idBecas`,`idAlumno`,`fechaInicio`) 
+    VALUES (0,1,2,'2020-12-20');
+    INSERT INTO alumnos.alumnobecas (`id`,`idBecas`,`idAlumno`,`fechaInicio`) 
+    VALUES (0,1,2,'2020-12-20');
+    INSERT INTO alumnos.alumnobecas (`id`,`idBecas`,`idAlumno`,`fechaInicio`) 
+    VALUES (0,1,2,'2020-12-20');
+    INSERT INTO alumnos.alumnobecas (`id`,`idBecas`,`idAlumno`,`fechaInicio`) 
+    VALUES (0,1,2,'2020-12-20');
+    INSERT INTO alumnos.alumnobecas (`id`,`idBecas`,`idAlumno`,`fechaInicio`) 
+    VALUES (0,1,2,'2020-12-20');
+    INSERT INTO alumnos.alumnobecas (`id`,`idBecas`,`idAlumno`,`fechaInicio`) 
+    VALUES (0,1,2,'2020-12-20');
+    INSERT INTO alumnos.alumnobecas (`id`,`idBecas`,`idAlumno`,`fechaInicio`) 
+    VALUES (0,1,2,'2020-12-20');
+    INSERT INTO alumnos.alumnobecas (`id`,`idBecas`,`idAlumno`,`fechaInicio`) 
+    VALUES (0,1,2,'2020-12-20');
+
+SAVEPOINT OchoRegistros;
+
+#Eliminar savepoint:
+
+# RELEASE SAVEPOINT CuatroRegistros;
+
+
+
+
+
